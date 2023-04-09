1cf3a8a8fa73732044b44d49eca1538c724b0b5f
josh-chief <yvoshngumi@gmail.com>
2023-04-09 23:24:52 +0300

    start

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
+ 
+ class MyApp extends StatelessWidget {
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      routes: (
+        "/":(context) =>HomePage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
- 
