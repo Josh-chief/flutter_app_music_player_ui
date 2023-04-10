b04ceacd277d2f4f4700135ef156f2536550a01f
josh-chief <yvoshngumi@gmail.com>
2023-04-10 22:14:17 +0300

    startpointHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_music_player/pages/HomePage.dart';
 
 void main() => runApp(MyApp());
- 
- class MyApp extends StatelessWidget {
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: (
-        "/":(context) =>HomePage(),
-      ),
+      routes: {
+        "/": (context) => HomePage(),
+      },
     );
   }
 }
---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(),
+    );
+  }
+}
