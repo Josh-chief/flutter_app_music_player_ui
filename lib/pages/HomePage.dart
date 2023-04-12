f7a4ea414ae4664924ca885f46c4d2792a67060d
josh-chief <yvoshngumi@gmail.com>
2023-04-12 22:32:28 +0300

    addedFeature

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -6,7 +6,43 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Scaffold(),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(top: 20, left: 22),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Icon(
+                          Icons.sort_rounded,
+                          color: Color(0xFF899CCF),
+                          size: 30,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Icon(
+                          Icons.more_vert,
+                          color: Color(0xFF899CCF),
+                          size: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
