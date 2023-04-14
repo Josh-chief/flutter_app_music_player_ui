8633084ce544aa733ec824922c06db6023fcfae2
josh-chief <yvoshngumi@gmail.com>
2023-04-15 02:06:43 +0300

    homepageError

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,27 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/widgets/MusicList.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    var musicList = MusicList;
+    return DefaultTabController(
+      length: 6,
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: SafeArea(
           child: Padding(
-            padding: EdgeInsets.only(top: 20, left: 22),
+            padding: const EdgeInsets.only(top: 20, left: 22),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [
+              children: [
                 Padding(
-                  padding: EdgeInsets.only(right: 15),
+                  padding: const EdgeInsets.only(right: 15),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       InkWell(
                         onTap: () {},
-                        child: Icon(
+                        child: const Icon(
                           Icons.sort_rounded,
                           color: Color(0xFF899CCF),
                           size: 30,
@@ -29,7 +32,7 @@ class HomePage extends StatelessWidget {
                       ),
                       InkWell(
                         onTap: () {},
-                        child: Icon(
+                        child: const Icon(
                           Icons.more_vert,
                           color: Color(0xFF899CCF),
                           size: 30,
@@ -38,9 +41,9 @@ class HomePage extends StatelessWidget {
                     ],
                   ),
                 ),
-                SizedBox(height: 30),
+                const SizedBox(height: 30),
                 Padding(
-                  padding: EdgeInsets.only(bottom: 5),
+                  padding: const EdgeInsets.only(bottom: 5),
                   child: Text(
                     "Hello Sir",
                     style: TextStyle(
@@ -51,6 +54,107 @@ class HomePage extends StatelessWidget {
                     ),
                   ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 5),
+                  child: Text(
+                    "What ypu want to hear sir?",
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.5),
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 15, right: 20, bottom: 20),
+                  child: Container(
+                    height: 50,
+                    width: 380,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF31314F),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.symmetric(horizontal: 15),
+                            height: 50,
+                            width: 200,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                hintText: "Search the Music",
+                                hintStyle: TextStyle(
+                                  color: Colors.white.withOpacity(0.5),
+                                ),
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 10),
+                              child: Icon(
+                                Icons.search,
+                                size: 30,
+                                color: Colors.white.withOpacity(0.5),
+                              )),
+                        ]),
+                  ),
+                ),
+                TabBar(
+                  isScrollable: true,
+                  labelStyle: TextStyle(fontSize: 18),
+                  indicator: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        width: 3,
+                        color: Color(0xFF31314F),
+                      ),
+                    ),
+                  ),
+                  tabs: [
+                    Tab(text: "Musics"),
+                    Tab(
+                      text: "Playlists",
+                    ),
+                    Tab(
+                      text: "Trending",
+                    ),
+                    Tab(
+                      text: "New",
+                    ),
+                    Tab(
+                      text: "Trending",
+                    ),
+                    Tab(
+                      text: "Collection",
+                    ),
+                  ],
+                ),
+                Flexible(
+                    flex: 1,
+                    child: TabBarView(
+                      children: [
+                        MusicList(),
+                        Container(
+                          color: Colors.red,
+                        ),
+                        Container(
+                          color: Colors.red,
+                        ),
+                        Container(
+                          color: Colors.red,
+                        ),
+                        Container(
+                          color: Colors.red,
+                        ),
+                        Container(
+                          color: Colors.red,
+                        ),
+                        Container(
+                          color: Colors.red,
+                        ),
+                      ],
+                    ))
               ],
             ),
           ),
---
+++ widgets/MusicList.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MusicList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.yellow);
+  }
+}
