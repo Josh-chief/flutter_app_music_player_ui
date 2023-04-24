f689de5caf9d3cbb8d1764b829d457e51f63b800
josh-chief <yvoshngumi@gmail.com>
2023-04-24 21:06:42 +0300

    musicpage

--- lib/pages/MusicPage.dart
+++ lib/pages/MusicPage.dart
@@ -7,7 +7,67 @@ class MusicPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Scaffold(),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage(
+              "image/background.jpg",
+            ),
+            fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.black.withOpacity(0.3),
+                Colors.black.withOpacity(0.5),
+                const Color(0xFF31314F).withOpacity(1),
+                const Color(0xFF31314F).withOpacity(1),
+              ],
+            ),
+          ),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 45, horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          CupertinoIcons.back,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          Icons.more_vert,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
